76ba994ff9447aa7e5dfa84c8a1e9fbd56d9248f
Rajkumar0503 <kumar.rajmca.raj@gmail.com>
2017-01-01 11:43:14 +0530

    Initial Code

---
+++ src/test/groovy/myFirstSpock.groovy
@@ -0,0 +1,29 @@
+import spock.lang.Specification
+
+class myFirstSpock extends Specification {
+	
+		def setup()
+		{
+			println "setuppppppppppppppppppppppppp"
+		}
+		
+		def cleanup()
+		{
+			println "cleanupppppppppppppppppppppp"
+		}
+		
+		def "my"(int a)
+		{
+			
+			expect:
+			a==2
+			
+			where:
+			a|_
+			2|_
+			2|_
+			2|_
+			
+		}
+
+}
